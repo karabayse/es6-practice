9337eff50cc2f1ea9ee7b278e1c6a5f2e5c40ca6
Kara Bayse <karabayse@gmail.com>
2017-10-14 10:28:09 -0500

    combined export statements

--- codecademy-es6.js
+++ codecademy-es6.js
@@ -460,3 +460,30 @@ function isLowSodium() {
 };
 // variable followed by isVegetarian function object
 export { specialty, isVegetarian };
+
+
+
+// combining export statements
+let specialty = '';
+function isVegetarian() {
+};
+function isLowSodium() {
+};
+function isGlutenFree() {
+};
+
+export { specialty as chefsSpecial, isVegetarian as isVeg };
+export default isGlutenFree;
+
+
+export let Menu = {};
+
+export let specialty = '';
+export let isVegetarian = function() {
+};
+export let isLowSodium = function() {
+};
+let isGlutenFree = function() {
+};
+
+export default isGlutenFree;
