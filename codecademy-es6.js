a3dceb76a7e59545fe70b4ee48f51316e5473f49
Kara Bayse <karabayse@gmail.com>
2017-10-17 09:38:30 -0500

    fizzbuzz

--- codecademy-es6.js
+++ codecademy-es6.js
@@ -487,3 +487,25 @@ let isGlutenFree = function() {
 };
 
 export default isGlutenFree;
+
+
+
+// FizzBuzz
+// Print out the numbers from 1 - 20.
+// For numbers divisible by 3, print out "Fizz".
+// For numbers divisible by 5, print out "Buzz".
+// For numbers divisible by both 3 and 5, print out "FizzBuzz" in the console.
+// Otherwise, just print out the number.
+for (i = 1; i < 21; i++) {
+  if (i % 3 === 0 && i % 5 !== 0) {
+    console.log("Fizz");
+  } else if
+    (i % 3 !== 0 && i % 5 === 0) {
+    console.log("Buzz");
+  } else if
+    (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else {
+    console.log(i);
+  }
+}
