e8dda2f7213f95b0616a3eb5f5e096fe66e7635c
Kara Bayse <karabayse@gmail.com>
2017-10-17 09:44:19 -0500

    GET and POST requests

--- codecademy-es6.js
+++ codecademy-es6.js
@@ -509,3 +509,34 @@ for (i = 1; i < 21; i++) {
     console.log(i);
   }
 }
+
+
+
+// GET and POST Requests
+// GET
+const xhr = new XMLHttpRequest();
+const url = 'http://api-to-call.com/endpoint';
+xhr.responseType = 'json';
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    console.log(xhr.response);
+  }
+};
+xhr.open('GET', url);
+xhr.send();
+
+
+// POST
+const xhr = new XMLHttpRequest();
+const url = 'http://api-to-call.com/endpoint';
+const data = JSON.stringify({
+  id: '200'
+});
+xhr.responseType = 'json';
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    console.log(xhr.response);
+  }
+};
+xhr.open('POST', url);
+xhr.send(data);
